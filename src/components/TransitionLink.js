eec6d92c433e8395c6032b82fdd324107f17506d
1papaya <1love1papaya@gmail.com>
2020-06-06 23:24:31 +0100

    @reach/router

--- src/components/TransitionLink.js
+++ src/components/TransitionLink.js
@@ -1,89 +1,99 @@
-import React, { forwardRef } from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import React, { forwardRef } from "react";
+import PropTypes from "prop-types";
+import { Link } from "@reach/router";
 
-import { shouldNavigate } from '../utils/shouldNavigate'
-import { triggerTransition } from '../utils/triggerTransition'
-import { Consumer } from '../context/createTransitionContext'
+import { shouldNavigate } from "../utils/shouldNavigate";
+import { triggerTransition } from "../utils/triggerTransition";
+import { Consumer } from "../context/createTransitionContext";
 
-if (typeof forwardRef === 'undefined') {
-	forwardRef = C => C
+if (typeof forwardRef === "undefined") {
+  forwardRef = (C) => C;
 }
 
 const TransitionLink = forwardRef(
-	(
-		{
-			to,
-			children,
-			exit,
-			entry,
-			activeStyle,
-			partiallyActive,
-			style,
-			className,
-			activeClassName,
-			state,
-			onClick,
-			trigger,
-			replace,
-			innerRef,
-			preventScrollJump,
-			...rest
-		},
-		ref
-	) => {
-		return (
-			<Consumer>
-				{({ disableAnimation, ...context }) => (
-					<Link
-						style={style}
-						activeStyle={activeStyle}
-						className={className}
-						activeClassName={activeClassName}
-						partiallyActive={partiallyActive}
-						onClick={event => {
-							// If the user has set their browser or OS to prefers-reduced-motion
-							// we should respect that.
-							if (disableAnimation) {
-								return
-							}
+  (
+    {
+      to,
+      children,
+      exit,
+      entry,
+      activeStyle,
+      partiallyActive,
+      style,
+      className,
+      activeClassName,
+      state,
+      onClick,
+      trigger,
+      replace,
+      innerRef,
+      preventScrollJump,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <Consumer>
+        {({ disableAnimation, ...context }) => (
+          <Link
+            style={style}
+            className={className}
+            onClick={(event) => {
+              // If the user has set their browser or OS to prefers-reduced-motion
+              // we should respect that.
+              if (disableAnimation) {
+                return;
+              }
 
-							const weShouldNavigate = shouldNavigate(event)
+              const weShouldNavigate = shouldNavigate(event);
 
-							if (weShouldNavigate) {
-								triggerTransition({
-									event,
-									to,
-									exit,
-									entry,
-									trigger,
-									replace,
-									preventScrollJump,
-									linkState: state,
-									...context,
-								})
-							}
+              if (weShouldNavigate) {
+                triggerTransition({
+                  event,
+                  to,
+                  exit,
+                  entry,
+                  trigger,
+                  replace,
+                  preventScrollJump,
+                  linkState: state,
+                  ...context,
+                });
+              }
 
-							if (typeof onClick === 'function') {
-								onClick(event, weShouldNavigate)
-							}
-						}}
-						ref={ref || innerRef}
-						{...rest}>
-						{children}
-					</Link>
-				)}
-			</Consumer>
-		)
-	}
-)
+              if (typeof onClick === "function") {
+                onClick(event, weShouldNavigate);
+              }
+            }}
+            getProps={({ isPartiallyCurrent, isCurrent }) => {
+              if (partiallyActive ? isPartiallyCurrent : isCurrent) {
+                return {
+                  className: [className, activeClassName]
+                    .filter(Boolean)
+                    .join(` `),
+                  style: { ...style, ...activeStyle },
+                };
+              }
+              return null;
+            }}
+            ref={ref || innerRef}
+            to={to}
+            {...rest}
+          >
+            {children}
+          </Link>
+        )}
+      </Consumer>
+    );
+  }
+);
 
 TransitionLink.propTypes = {
-	to: PropTypes.string.isRequired,
-	exitLength: PropTypes.number,
-	entryDelay: PropTypes.number,
-	exitFn: PropTypes.func,
-	entryState: PropTypes.object,
-}
+  to: PropTypes.string.isRequired,
+  exitLength: PropTypes.number,
+  entryDelay: PropTypes.number,
+  exitFn: PropTypes.func,
+  entryState: PropTypes.object,
+};
 
-export { TransitionLink }
+export { TransitionLink };
